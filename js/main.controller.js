f054c69ee642a67f851e9332617301a59a503647
sellers1894 <sellers1894@yandex.ru>
2016-10-30 23:46:28 +0300

    Начальная структура ф-в

--- js/main.controller.js
+++ js/main.controller.js
@@ -18,6 +18,10 @@
       }).then(function(res2){
         vm.step2 = res2;
         console.log(vm.step2);
+        return dataservice.getData('meet/'+vm.step2.meet);
+      }).then(function(res3){
+        vm.step3 = res3;
+        console.log(vm.step3);
       });
     }
   }
