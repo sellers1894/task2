3382502cc502e272120d8cfe011db7c447380433
sellers1894 <sellers1894@yandex.ru>
2016-11-03 15:46:59 +0300

    localStorage
    добавляются все json в localStorage

--- js/dataservice.service.js
+++ js/dataservice.service.js
@@ -1,9 +1,5 @@
 (function() {
-  angular
-  .module('app.helpers').config(function ($httpProvider, $httpParamSerializerJQLikeProvider){
-    $httpProvider.defaults.transformRequest.unshift($httpParamSerializerJQLikeProvider.$get());
-    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=utf-8';
-  });
+
   angular
   .module('app.helpers')
   .factory('dataservice', Dataservice);
@@ -11,27 +7,72 @@
   Dataservice.$inject = ['$http', '$q']
 
   function Dataservice($http, $q) {
-    var users = ['john', 'andrey', 'kate'];
+    var usersName = ['john', 'andrey', 'kate'];
+    var todolistName = ['list1', 'list2', 'list3'];
+    var meetName = ['meet1', 'meet2', 'meet3'];
     var service = {
+      onLoad: onLoad,
+      getDataJSON: getDataJSON,
       getData: getData,
       setData: setData,
       getRandomUser: getRandomUser
     };
     return service;
 
-    function getRandomUser(){
-      return users[Math.floor(Math.random()*users.length)];
+    function onLoad(){
+      localStorage.setItem("usersName", JSON.stringify(usersName));
+      localStorage.setItem("todolistName", JSON.stringify(todolistName));
+      localStorage.setItem("meetName", JSON.stringify(meetName));
+
+      var users = [];
+      usersName.forEach(function(item) {
+        users.push(getDataJSON('users/'+item));
+      });
+      localStorage.setItem("users", JSON.stringify(users));
+
+      var todolist = [];
+      todolistName.forEach(function(item) {
+        todolist.push(getDataJSON('todolist/'+item));
+      });
+      localStorage.setItem("todolist", JSON.stringify(todolist));
+
+      setDataArrToLocal(usersName, 'users');
+      setDataArrToLocal(todolistName, 'todolist');
+      setDataArrToLocal(meetName, 'meet');
+      console.log(JSON.parse(localStorage.getItem('users.john')));
+
+
     }
 
-    function getData(path) {
-      $http
+    function getRandomUser(){
+      var buf = JSON.parse(localStorage.getItem("usersName"));
+      return usersName[Math.floor(Math.random()*buf.length)];
+    }
+
+    function setDataArrToLocal(arrName, type){
+      arrName.forEach(function(item) {
+        getDataJSON(type+'/'+item).then(function(res) {
+          localStorage.setItem(type+'.'+item, JSON.stringify(res));
+        });
+      });
+    }
+
+    function getDataJSON(path) {
+      // console.log(path);
       return $http.get('data/'+path+'.json').then(function(res) {
         return res.data.testData;
       })
     }
 
+    function getData(type, name) {
+      return JSON.parse(localStorage.getItem(type+'.'+name));
+      // return $http.get('data/'+path+'.json').then(function(res) {
+      //   return res.data.testData;
+      // })
+    }
+
     function setData(){
-      
+
     }
   }
 })();
--- js/main.controller.js
+++ js/main.controller.js
@@ -13,15 +13,18 @@
     vm.reverse = false;
 
     function activate() {
+      dataservice.onLoad();
+      localStorage.setItem("saved", "ad");
+      console.log(localStorage.getItem("saved"));
       var user = dataservice.getRandomUser();
-      dataservice.getData('users/'+user).then(function(res) {
+      dataservice.getDataJSON('users/'+user).then(function(res) {
         vm.step1 = res;
         console.log(vm.step1);
-        return dataservice.getData('todolist/'+vm.step1.todoListUrl);
+        return dataservice.getDataJSON('todolist/'+vm.step1.todoListUrl);
       }).then(function(res2){
         vm.step2 = res2;
         console.log(vm.step2);
-        return dataservice.getData('meet/'+vm.step2.meet);
+        return dataservice.getDataJSON('meet/'+vm.step2.meet);
       }).then(function(res3){
         vm.step3 = res3;
         console.log(vm.step3);
